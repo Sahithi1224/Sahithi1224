0d958f4940d218df3ca2aa711223b3fd80cb43dc
Sahithi1224 <112922087+Sahithi1224@users.noreply.github.com>
2022-10-06 11:12:10 +0530

    Add files via upload

---
+++ area_of_rectangle.py
@@ -0,0 +1,5 @@
+#Area_of_rectangle
+length=int(input("Enter the length of the rectangle: "))
+breadth=int(input("Enter the breadth of the rectangle: "))
+area =  length*breadth
+print("Area of rectangle is ",area)
